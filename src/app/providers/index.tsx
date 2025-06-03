242a98058499c02647701a9042d90c29aad6bbf9
gineff <canone@inbox.ru>
2025-06-03 13:00:54 +0300

    ft: add app slice with providers

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,6 @@
+import compose from './compose';
+
+/** Используем функцию compose для создания HOC withProviders,
+который последовательно оборачивает компонент в провайдеры маршрутизатора, хранилища и темы. */
+
+export const withProviders = compose();
