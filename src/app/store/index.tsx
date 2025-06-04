e4b37bb60bbf5254db9446d16866bef9cb2f6b47
gineff <canone@inbox.ru>
2025-06-04 08:32:54 +0300

    ft: add thunk middleware

--- src/app/store/index.tsx
+++ src/app/store/index.tsx
@@ -1,9 +1,16 @@
-import { useReducer, type ReactNode } from 'react';
+import { useCallback, useMemo, useReducer, type Dispatch, type ReactNode } from 'react';
 import { initialState } from './initial-state';
 import { todoReducer } from '@/entities/todo/model/reducer';
 import type { TodoState } from '@/entities/todo/model/types';
-import type { ReducersMapObject, RootAction } from './types';
+import type { Middleware, MiddlewareAPI, ReducersMapObject, RootAction } from './types';
 import { StoreContext } from '@/shared/lib/store/context';
+
+const compose = <S, A>(...middlewares: Middleware<S, A>[]) => {
+  return (store: MiddlewareAPI<S, A>) => {
+    const chain = middlewares.map((middleware) => middleware(store));
+    return (next: Dispatch<A>) => chain.reduceRight((acc, middleware) => middleware(acc), next);
+  };
+};
 
 const combineReducers =
   <S, A>(reducers: ReducersMapObject<S, A>) =>
@@ -15,17 +22,45 @@ const combineReducers =
     return newState;
   };
 
+const thunk =
+  <S, A>({ dispatch, getState }: MiddlewareAPI<S, A>) =>
+  (next: Dispatch<A>) =>
+  (action: any, ...args: unknown[]) => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState, ...args);
+    }
+    return next(action);
+  };
+
+
 interface StoreProviderProps {
   children: ReactNode;
 }
 
 export const StoreProvider = ({ children }: StoreProviderProps) => {
-  const [state, dispatch] = useReducer(
-    combineReducers<{ todo: TodoState;}, RootAction>({
-      todo: todoReducer,
+  const rootReducer = combineReducers<{ todo: TodoState }, RootAction>({
+    todo: todoReducer,
+  });
+
+  const [state, originalDispatch] = useReducer(rootReducer, initialState);
+
+  const getState = useCallback(() => state, [state]);
+
+  const middlewareAPI: MiddlewareAPI<{ todo: TodoState }, RootAction> = {
+    getState,
+    dispatch: (action: RootAction) => enhancedDispatch(action),
+  };
+
+  const applyMiddleware = compose<{ todo: TodoState }, RootAction>(thunk);
+  const enhancedDispatch = applyMiddleware(middlewareAPI)(originalDispatch);
+
+  const contextValue = useMemo(
+    () => ({
+      state,
+      dispatch: enhancedDispatch,
     }),
-    initialState,
+    [state, enhancedDispatch],
   );
 
-  return <StoreContext.Provider value={{ state, dispatch }}>{children}</StoreContext.Provider>;
+  return <StoreContext.Provider value={contextValue}>{children}</StoreContext.Provider>;
 };
--- src/app/store/types.ts
+++ src/app/store/types.ts
@@ -4,8 +4,19 @@ export type RootState = {
   todo: TodoState;
 };
 
-export type RootAction = TodoAction ; // | OtherAction | AnotherAction
+export type RootAction = TodoAction; // | OtherAction | AnotherAction
 
 export type ReducersMapObject<S, A> = {
   [K in keyof S]: (state: S[K], action: A) => S[K];
 };
+
+export type Middleware<S, A> = (
+  store: MiddlewareAPI<S, A>,
+) => (next: Dispatch<A>) => (action: A) => unknown;
+
+export type Dispatch<A> = (action: A) => void;
+
+export interface MiddlewareAPI<S, A> {
+  getState: () => S;
+  dispatch: Dispatch<A>;
+}
